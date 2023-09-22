7c290b6e77b93b6ac2e58e4b21dba0cd0d735d0e
Cyril Roos <cyril@info.nl>
2023-09-22 16:49:15 +0200

    changed setting of variable method

--- operations/_scripts/generate/generate_tf_vars.sh
+++ operations/_scripts/generate/generate_tf_vars.sh
@@ -72,6 +72,10 @@ aws_resource_identifier="aws_resource_identifier = \"${GITHUB_IDENTIFIER}\""
 aws_resource_identifier_supershort="aws_resource_identifier_supershort = \"${GITHUB_IDENTIFIER_SS}\""
 aws_security_group_name_pg="aws_security_group_name_pg = \"${GITHUB_IDENTIFIER}-pg\""
 
+# docker repository credentials
+registry_username="registry_username \"${REGISTRY_USERNAME}\""
+registry_password="registry_password \"${REGISTRY_PASSWORD}\""
+
 # Special cases
 
 ec2_iam_instance_profile=
@@ -160,9 +164,6 @@ data_mount_target=$(generate_var data_mount_target $DATA_MOUNT_TARGET)
 ec2_volume_size=$(generate_var ec2_volume_size $EC2_VOLUME_SIZE)
 docker_remove_orphans=$(generate_var docker_remove_orphans $DOCKER_REMOVE_ORPHANS)
 docker_force_build=$(generate_var docker_force_build $DOCKER_FORCE_BUILD)
-# docker repository credentials
-registry_username=$(generate_var registry_username $REGISTRY_USERNAME)
-registry_password=$(generate_var registry_password $REGISTRY_PASSWORD)
 
 # -------------------------------------------------- #
 
