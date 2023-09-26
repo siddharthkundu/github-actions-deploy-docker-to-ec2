16167bcc14ca2fd7111ad128337186f20ee399f2
Cyril Roos <cyril@info.nl>
2023-09-26 16:26:40 +0200

    duplicate

--- operations/deployment/terraform/security-group.tf
+++ operations/deployment/terraform/security-group.tf
@@ -1,7 +1,3 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_security_group" "ec2_security_group" {
   name        = var.aws_resource_identifier
   description = "SG for ${var.aws_resource_identifier}"
