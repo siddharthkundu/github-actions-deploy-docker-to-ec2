0e355c1c5ae91f446d14d2a95d39953afec84417
Cyril Roos <cyril@info.nl>
2023-09-26 11:31:36 +0200

    custom sg

--- operations/deployment/terraform/security-group.tf
+++ operations/deployment/terraform/security-group.tf
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "ingress_http" {
   from_port         = tonumber(var.app_port)
   to_port           = tonumber(var.app_port)
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["80.79.194.23/32","80.79.194.3/32"]
   security_group_id = aws_security_group.ec2_security_group.id
 }
 
@@ -34,7 +34,7 @@ resource "aws_security_group_rule" "ingress_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["80.79.194.23/32","80.79.194.3/32"]
   security_group_id = aws_security_group.ec2_security_group.id
 }
 
@@ -45,6 +45,6 @@ resource "aws_security_group_rule" "ingress_lb_port" {
   from_port         = tonumber(var.lb_port != "" ? var.lb_port : (local.cert_available ? 443 : 80))
   to_port           = tonumber(var.lb_port != "" ? var.lb_port : (local.cert_available ? 443 : 80))
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["80.79.194.23/32","80.79.194.3/32","81.204.57.46/32","143.178.87.144/32","129.126.185.224/32","129.126.203.153/32"]
   security_group_id = aws_security_group.ec2_security_group.id
 }
