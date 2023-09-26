6d42bc78b18a87277175edc04a07c098e63c506d
Cyril Roos <cyril@info.nl>
2023-09-26 11:48:55 +0200

    for now open up ssh for the github runner to connect

--- operations/deployment/terraform/security-group.tf
+++ operations/deployment/terraform/security-group.tf
@@ -34,7 +34,7 @@ resource "aws_security_group_rule" "ingress_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["80.79.194.23/32","80.79.194.3/32"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ec2_security_group.id
 }
 
